97deb5928259414689b05a3d951b2b101e3f66af
YulyaChervakova <yulya.chervakova@gmail.com>
2021-04-12 01:54:59 +0300

    п

--- Js.js
+++ Js.js
@@ -1,115 +1,159 @@
 //Таблица
-// function tabBoyer(arrSearch) {
-//     var arrTab = [
-//         [],
-//         []
-//     ];
-//     var flag = 1;
-//     var i = arrSearch.length - 1;
-    
-//     // заполнение таблицы
-//     for (var j = 0; j < arrSearch.length; j++) {
-//         arrTab[0][j] = arrSearch[i];
-//         arrTab[1][j] = j;
-//         i--;
-//     }
-
-
-
-
-//     //  console.log(arrTab);
-//     //Удаленипе повторяющихся элементов 
-//     for (var j = 0; j < arrSearch.length; j++) {
-
-//         for (var m = j + 1; m < arrSearch.length; m++) {
-//             if (arrTab[0][j] == arrTab[0][m]) {
-//                 if (arrTab[0][0] == arrTab[0][m] && flag == 1) {
-//                     arrTab[1][0] = arrTab[1][m];
-//                     arrTab[0].splice(m, 1);
-//                     arrTab[1].splice(m, 1);
-
-//                     flag++;
-//                 } else {
-//                     arrTab[0].splice(m, 1);
-//                     arrTab[1].splice(m, 1);
-                    
-//                 }
-
-//             }
-//         }
-
-//     }
-//     console.log(arrTab);
-//     console.log(i);
-//     //Последний элемент
-//     // for (var j = 1; j < arrSearch.length ; j++) {
-//     //     if (myArr[0][j] == arrTab[0][0]) {
-//     //         arrTab[0].splice(j, 1); 
-//     //         arrTab[1].splice(j, 1); 
-//     //         arrTab[1][0]=j;
-//     //         console.log(j);
-//     //         if(arrTab[0][0]==)///прописать иф длля для удаления лишний копии 1 элемента
-
-//     //         return arrTab;
-//     //     }
-//     // }
-
-//     //console.log(arrTab);
-//     //console.log(i);
-//     if (flag == 1) {
-//         arrTab[1][0] = arrSearch.length;
-//         return arrTab;
-//     } else {
-//         return arrTab;
-//     }
-
-
-// }
-
 function tabBoyer(arrSearch) {
     const arrMap = new Map();
     var arrReverse = arrSearch.split("").reverse().join("");
 
-    for(var i = 1; i < arrReverse.length; i++){
-        if(arrMap.get(arrReverse[i]) == undefined){
+    for (var i = 1; i < arrReverse.length; i++) {
+        if (arrMap.get(arrReverse[i]) == undefined) {
             arrMap.set(arrReverse[i], i);
         }
     }
 
-    if(arrMap.get(arrReverse[0]) == undefined){
+    if (arrMap.get(arrReverse[0]) == undefined) {
         arrMap.set(arrReverse[0], arrReverse.length)
+    } else {
+        arrMap.set('*', arrReverse.length)
     }
 
     return arrMap;
 }
 
+function boyer(arrSearch, arrMap, arrOutput) {
+    var i = arrSearch.length - 1;
 
+    while (i < arrOutput.length) {
+        if (arrMap.get(arrOutput[i]) == undefined) {
+            if (arrMap.get('*') == undefined) {
+                i += arrMap.get(arrSearch[arrSearch.length - 1]);
+
+            } else {
+                i = i + arrMap.get('*');
+            }
+        } else {
+            if (arrOutput[i] == arrSearch[arrSearch.length - 1]) {
+                var k = arrSearch.length - 1;
+                for (var j = i; j > i - arrSearch.length - 1; j--) {
+                    if (arrOutput[j] == arrSearch[k] && k >= 0) {
+                        k--;
+                    } else {
+                        if (k == -1) {
+                            return i;
+                        } else {
+                            i += arrMap.get(arrSearch[k]);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                i += arrMap.get(arrOutput[i]);
+            }
+        }
+    }
+    return -1;
+}
 var strOutput = document.getElementById('strOutput');
-strOutput.value = "Оценить время работы";
+strOutput.value = "Оценить времмяя работы";
 
 var inputStr = document.getElementById('inputStr');
-var str = strOutput.value; //удалить
+
 
 
 //Добавление строк
 AddStr1.onclick = function () {
     strOutput.value += inputStr.value;
     inputStr.value = '';
-    str = strOutput.value; //удалить
+
 }
 
 
-//Поиск -Упрощенный Бойера-Мура
+//Поиск -Упрощенный Бойера-Мура+ Стандарный поиск // с учетом регистра 
+
+
 SearchStr.onclick = function () {
     var strSerch = document.getElementById('strSerch');
-    var tab = tabBoyer(inputStr.value);
 
-    var result = "";
+    if (inputStr.value.length <= strOutput.value.length) {
+        var tab = tabBoyer(inputStr.value);
+        var result = "";
+        var start = new Date().getTime();
+        for (var i = 0; i < 10000; i++) {
+            result = boyer(inputStr.value, tab, strOutput.value);
+        }
+        var end = new Date().getTime();
+        if (result == -1) {
+            strSerch.innerHTML = "<div><p>Упрощенный Бойера-Мура:Подстрака не найдена</p></div>"
+        } else {
+            //Вывод
+            var textFull, textFull2, textSearch;
+            textFull = strOutput.value.slice(0, result - inputStr.value.length + 1);
+            textSearch = strOutput.value.slice(result + 1 - inputStr.value.length, result + 1);
+            textFull2 = strOutput.value.slice(result + 1, strOutput.value.length);
+            strSerch.innerHTML = "<div><p>Упрощенный Бойера-Мура</p><p>" + textFull + "<s>" + textSearch + "</s>" + textFull2 + "</p>" + "<p>Время работы:" + (end - start) / 10000 + "ms</p></div>";
+        }
 
-    for (var [key, value] of tab) {
-        result += key + ' = ' + value + "<br>";
+    } else {
+        strSerch.innerHTML = "<p>Упрощенный Бойера-Мура:Подстрока не найдена</p>";
+    }
+    // Стандартный поиск 
+    start = new Date().getTime();
+    for (var i = 0; i < 10000; i++) {
+        var standartSearch = strOutput.value.indexOf(inputStr.value, 0);
+    }
+    end = new Date().getTime();
+    //Вывод
+    if (standartSearch >= 0) {
+        textFull = strOutput.value.slice(0, standartSearch);
+        textSearch = strOutput.value.slice(standartSearch, standartSearch + inputStr.value.length);
+        textFull2 = strOutput.value.slice(standartSearch + inputStr.value.length, strOutput.value.length);
+        strSerch.innerHTML += "<div><p>Cтандартнаяй функция поиска</p><p>" + textFull + "<s>" + textSearch + "</s>" + textFull2 + "</p>" + "<p>Время работы:" + (end - start) / 10000 + "ms</p></div>";
+    } else {
+        strSerch.innerHTML += "<p><div>Cтандартной функции поиска: Подстрока не найдена</p></div>";
+
     }
 
-    strSerch.innerHTML = "<p>:" + result + "</p>"
 }
-///C
+//Поиск -Упрощенный Бойера-Мура+ Стандарный поиск // без учетом регистра 
+SearchStr2.onclick = function () {
+    var strSerch = document.getElementById('strSerch');
+ var strCaps = inputStr.value.toUpperCase();
+ var strOutputCaps=strOutput.value.toUpperCase();
+    if (inputStr.value.length <= strOutput.value.length) {
+        var tab = tabBoyer(strCaps.value);
+        var result = "";
+        var start = new Date().getTime();
+        for (var i = 0; i < 10000; i++) {
+            result = boyer(strCaps, tab, strOutputCaps);
+        }
+        var end = new Date().getTime();
+        if (result == -1) {
+            strSerch.innerHTML = "<div><p>Упрощенный Бойера-Мура:Подстрака не найдена</p></div>"
+        } else {
+            //Вывод
+            var textFull, textFull2, textSearch;
+            textFull = strOutput.value.slice(0, result - inputStr.value.length + 1);
+            textSearch = strOutput.value.slice(result + 1 - inputStr.value.length, result + 1);
+            textFull2 = strOutput.value.slice(result + 1, strOutput.value.length);
+            strSerch.innerHTML = "<div><p>Упрощенный Бойера-Мура</p><p>" + textFull + "<s>" + textSearch + "</s>" + textFull2 + "</p>" + "<p>Время работы:" + (end - start) / 10000 + "ms</p></div>";
+        }
+
+    } else {
+        strSerch.innerHTML = "<div><p>Упрощенный Бойера-Мура:Подстрока не найдена</p></div>";
+    }
+    // Стандартный поиск 
+    start = new Date().getTime();
+    for (var i = 0; i < 10000; i++) {
+        var standartSearch = strOutputCaps.value.indexOf(strCaps, 0);
+    }
+    end = new Date().getTime();
+    //Вывод
+    if (standartSearch >= 0) {
+        textFull = strOutput.value.slice(0, standartSearch);
+        textSearch = strOutput.value.slice(standartSearch, standartSearch + inputStr.value.length);
+        textFull2 = strOutput.value.slice(standartSearch + inputStr.value.length, strOutput.value.length);
+        strSerch.innerHTML += "<div><p>Cтандартнаяй функция поиска</p><p>" + textFull + "<s>" + textSearch + "</s>" + textFull2 + "</p>" + "<p>Время работы:" + (end - start) / 10000 + "ms</p></div>";
+    } else {
+        strSerch.innerHTML += "<div><p>Cтандартной функции поиска: Подстрока не найдена</p></div>";
+
+    }
+
+}
